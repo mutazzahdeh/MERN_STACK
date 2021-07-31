e72da66ac320be61977f7f185b1cff0a45dc2bee
mutazzahdeh <mutazzahdeh@gmail.com>
2021-07-31 23:55:14 +0300

    full stack react

---
+++ Full Stack MERN/Product Manager(Part II)/server/controller/proudct.controller.js
@@ -0,0 +1,28 @@
+const { Product } = require('../models/product.model');
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+module.exports.findAllProduct = (req, res) => {
+    Product.find()
+      .then(product => res.json(product))
+      .catch(err => res.json({ message: "Something went wrong", error: err }));
+  };
+    // The method below is new
+module.exports.createProduct = (request, response) => {
+    const { title, price, desc } = request.body;
+    Product.create({
+        title,
+        price,
+        desc
+    })
+        .then(Product => response.json(Product))
+        .catch(err => response.json(err));
+}
+
+module.exports.getProduct = (request, response) => {
+    Product.findOne({_id:request.params.id})
+        .then(product => response.json(product))
+        .catch(err => response.json(err))
+}
